d9d15a6708ca5859ebe3fe274ef0ea712de64aa3
Dan Souza <Souza.music@outlook.com>
2022-03-07 22:27:18 -0800

    update

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-const EMPTY_HEART = '♡';
-const FULL_HEART = '♥';
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
-var like = document.getElementsByClassName("like", "like-glyph").addEventListener("click", likes);
-
-function likes() {
-  like.alert("hello");
+var likes = document.getElementsByClassName("like-glyph");
+for (let i = 0; i < likes.length; i++) {
+  likes[i].addEventListener("click", function (mimicServerCall) {
+    //alert("Success")
+    mimicServerCall = () => {
+      const myPromise = new promise((resolve, reject) => {
+        myPromise
+          .then(handleResolvedA, handleRejectedA)
+          .catch(handleRejectedAny);
+      });
+    };
+  });
 }
-
-/*function msc (mimicServerCall) {
-  debugger;  
-  return mimicServerCall
-  .then(() => {})
-  .catch(() => {})
-}*/
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
@@ -31,3 +31,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+document.querySelector(
+  "#react-root > section > main > section > div > div:nth-child(2) > div > article:nth-child(1) > div > div.qF0y9.Igw0E.IwRSH.eGOV_._4EzTm > div > div > section.ltpMr.Slqrh > span.fr66n > button > div.QBdPU.rrUvL > span > svg"
+);
